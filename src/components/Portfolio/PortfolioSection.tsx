55c3180e74eacbd976d59a4dfbb8c671648bed99
Dmitriy <dima-25500@mail.ru>
2023-07-22 20:51:17 +0300

    Portfolio carusel start

--- src/components/Portfolio/PortfolioSection.tsx
+++ src/components/Portfolio/PortfolioSection.tsx
@@ -1,11 +1,25 @@
 import '../../styles/portfolio.css';
+import '../../styles/portfolio_carusel.css';
+
+import MyButtonRounded from '../UI/MyButton/MyButtonRounded';
+import PortfolioCarusel from './PortfolioCarusel';
 
 function PortfolioSection() {
   return (
     <div className='portfolio__wrapper'>
-        <div className='portfolio__background portfolio__content__wrapper'>
-            
+      <div className='portfolio__background portfolio__content__wrapper'>
+
+        <div className='portfolio__content__text'>
+          <h1 className='portfolio__content__text_h1'>Наше портфолио</h1>
+          <p className='portfolio__content__text_p'>В данном портфолио вы сможете увидеть кейсы наших работ на 2022 - 2023 год</p>
+          <div>
+            <MyButtonRounded text='Компьютерные версии' />
+          </div>
         </div>
+
+      <PortfolioCarusel/>
+
+      </div>
     </div>
   );
 }
