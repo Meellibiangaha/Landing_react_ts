117252a911af4b9ae2f1338a34b0d092bca4058a
Dmitriy <dima-25500@mail.ru>
2023-08-10 15:45:37 +0300

    написал интерфейс SlideBlockProps для каруселей

--- src/components/Portfolio/CaruselFunck.tsx
+++ src/components/Portfolio/CaruselFunck.tsx
@@ -1,90 +1,64 @@
+import { FC } from "react";
+
+
 const valueChange = (value: number, unit: 'px' | 'rem' | 'em') => `${value}${unit}`;
 
-export function nextSlide({ position, width, caruselItem, list, countBlock, count, listElems }: any): { newPosition: number, newCountBlock: number } {
-    //new, т.к. я не смог просто запихнуть position, там реакт, состояния, все дела
-    let newPosition: number;
-    let newCountBlock: number;
-    if (position === -(width * caruselItem)) {
-        newPosition = 0;
-        list.style.marginLeft = valueChange(newPosition, 'px');
-        newCountBlock = 0;
-    } else {
-        newPosition = position - width * count;
-        newPosition = Math.max(newPosition, -width * (listElems.length - count));
-        list.style.marginLeft = valueChange(newPosition, 'px');
-        newCountBlock = countBlock + 1;
-    }
-    console.log(position);
-    console.log(width * caruselItem);
 
-    return { newPosition, newCountBlock };
+interface SlideBlockProps {
+    position: number;
+    width: number;
+    caruselItem: number;
+    list: HTMLElement;
+    countBlock: number;
+    count: number;
+    listElems?: NodeListOf<HTMLElement>;
 }
 
-export function prevSlide({ position, width, caruselItem, list, countBlock, count }: any): { newPosition: number, newCountBlock: number } {
-    let newPosition: number;
-    let newCountBlock: number;
-    if (position === 0) {
-        newPosition = -width * caruselItem;
-        list.style.marginLeft = valueChange(newPosition, 'px');
-        newCountBlock = caruselItem;
-    } else {
-        newPosition = position + width * count;
-        newPosition = Math.min(newPosition, 0);
-        list.style.marginLeft = valueChange(newPosition, 'px');
-        newCountBlock = countBlock - 1;
-    }
-    return { newPosition, newCountBlock };
-}
 
-//карусель для тарифов, пришлось дублировать половинный функционал
-//можно было костяк в отдельную функцию запихнуть
-export function nextSlideTarif({ position, width, caruselItem, list, countBlock, count, listElems }: any): { newPosition: number, newCountBlock: number } {
-    //new, т.к. я не смог просто запихнуть position, там реакт, состояния, все дела
-    let newPosition: number;
-    let newCountBlock: number;
-    const listElemsArr = Array.from(listElems);
+export const nextSlide =
+    ({
+        position,
+        width,
+        caruselItem,
+        list,
+        countBlock,
+        count,
+        listElems
+    }: SlideBlockProps) => {
+        if (listElems) {
+            //new, т.к. я не смог просто запихнуть position, там реакт, состояния, все дела
+            let newPosition: number;
+            let newCountBlock: number;
+            if (position === -(width * caruselItem)) {
+                newPosition = 0;
+                list.style.marginLeft = valueChange(newPosition, 'px');
+                newCountBlock = 0;
+            } else {
+                newPosition = position - width * count;
+                newPosition = Math.max(newPosition, -width * (listElems.length - count));
+                list.style.marginLeft = valueChange(newPosition, 'px');
+                newCountBlock = countBlock + 1;
+            }
 
-    if (countBlock === 0 || countBlock === listElemsArr.length - 2) {
-        newCountBlock = countBlock + 1;
-        newPosition = position - width * count;
-    }
-    else {
-        if (position <= -(width * caruselItem) + width) {
-            newPosition = 0;
-            list.style.marginLeft = valueChange(newPosition, 'px');
-            newCountBlock = 0;
-        } else {
-            newPosition = position - width * count;
-            newPosition = Math.max(newPosition, -width * (listElems.length - count));
-            list.style.marginLeft = valueChange(newPosition, 'px');
-            newCountBlock = countBlock + 1;
+            return { newPosition, newCountBlock };
         }
-    }
-    return { newPosition, newCountBlock };
-}
 
-export function prevSlideTarif({ position, width, caruselItem, list, countBlock, count, listElems }: any): { newPosition: number, newCountBlock: number } {
-    let newPosition: number;
-    let newCountBlock: number;
-    const listElemsArr = Array.from(listElems);
-
-    if (countBlock === 1 || countBlock === listElemsArr.length - 1) {
-        if (position === -width && countBlock === 1){
-            newPosition = 0;
-            list.style.marginLeft = valueChange(newPosition, 'px');
-        }
-            
-        else{
-            newPosition = position + width * count;
-        }
-        newCountBlock = countBlock - 1;
     }
 
-    else {
+export const prevSlide =
+    ({
+        position,
+        width,
+        caruselItem,
+        list,
+        countBlock,
+        count,
+    }: SlideBlockProps) => {
 
-        if (position >= 0) {
-            // чтобы был виден предпоследний тариф, прокручиваем как на 2
-            newPosition = (-width * caruselItem) + width;
+        let newPosition: number;
+        let newCountBlock: number;
+        if (position === 0) {
+            newPosition = -width * caruselItem;
             list.style.marginLeft = valueChange(newPosition, 'px');
             newCountBlock = caruselItem;
         } else {
@@ -93,8 +67,88 @@ export function prevSlideTarif({ position, width, caruselItem, list, countBlock,
             list.style.marginLeft = valueChange(newPosition, 'px');
             newCountBlock = countBlock - 1;
         }
+        return { newPosition, newCountBlock };
     }
 
+//карусель для тарифов, пришлось дублировать половинный функционал
+//можно было костяк в отдельную функцию запихнуть
+export const nextSlideTarif =
+    ({
+        position,
+        width,
+        caruselItem,
+        list,
+        countBlock,
+        count,
+        listElems
+    }: SlideBlockProps) => {
+        if (listElems) {
+            //new, т.к. я не смог просто запихнуть position, там реакт, состояния, все дела
+            let newPosition: number;
+            let newCountBlock: number;
+            const listElemsArr = Array.from(listElems);
 
-    return { newPosition, newCountBlock };
-}
+            if (countBlock === 0 || countBlock === listElemsArr.length - 2) {
+                newCountBlock = countBlock + 1;
+                newPosition = position - width * count;
+            }
+            else {
+                if (position <= -(width * caruselItem) + width) {
+                    newPosition = 0;
+                    list.style.marginLeft = valueChange(newPosition, 'px');
+                    newCountBlock = 0;
+                } else {
+                    newPosition = position - width * count;
+                    newPosition = Math.max(newPosition, -width * (listElems.length - count));
+                    list.style.marginLeft = valueChange(newPosition, 'px');
+                    newCountBlock = countBlock + 1;
+                }
+            }
+            return { newPosition, newCountBlock };
+        }
+    }
+
+export const prevSlideTarif =
+    ({
+        position,
+        width,
+        caruselItem,
+        list,
+        countBlock,
+        count,
+        listElems
+    }: SlideBlockProps) => {
+        if (listElems) {
+            let newPosition: number;
+            let newCountBlock: number;
+            const listElemsArr = Array.from(listElems);
+
+            if (countBlock === 1 || countBlock === listElemsArr.length - 1) {
+                if (position === -width && countBlock === 1) {
+                    newPosition = 0;
+                    list.style.marginLeft = valueChange(newPosition, 'px');
+                }
+
+                else {
+                    newPosition = position + width * count;
+                }
+                newCountBlock = countBlock - 1;
+            }
+
+            else {
+
+                if (position >= 0) {
+                    // чтобы был виден предпоследний тариф, прокручиваем как на 2
+                    newPosition = (-width * caruselItem) + width;
+                    list.style.marginLeft = valueChange(newPosition, 'px');
+                    newCountBlock = caruselItem;
+                } else {
+                    newPosition = position + width * count;
+                    newPosition = Math.min(newPosition, 0);
+                    list.style.marginLeft = valueChange(newPosition, 'px');
+                    newCountBlock = countBlock - 1;
+                }
+            }
+            return { newPosition, newCountBlock };
+        }
+    }
